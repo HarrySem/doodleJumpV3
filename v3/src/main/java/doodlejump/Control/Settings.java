8394d7f3f7d75e9b2ed4e30d28bb15fb89c93f26
Moritz Koebe <moritz.koebe@gmail.com>
2021-09-01 22:31:42 +0200

    added nicely working spring platform

--- v3/src/main/java/doodlejump/Control/Settings.java
+++ v3/src/main/java/doodlejump/Control/Settings.java
@@ -1,35 +1,39 @@
 package doodlejump.Control;
 
 public class Settings {
-    public static double SCENE_WIDTH = 1280;
-    public static double SCENE_HEIGHT = 720;
+    public static final double SCENE_WIDTH = 1280;
+    public static final double SCENE_HEIGHT = 720;
 
-    public static int ATTRACTOR_COUNT = 1;
-    public static int VEHICLE_COUNT = 10;
+    public static final int ATTRACTOR_COUNT = 1;
+    public static final int VEHICLE_COUNT = 10;
 
-    public static double SPRITE_MAX_SPEED = 2;
-    public static double SPRITE_MAX_FORCE = 0.1;
+    public static final double SPRITE_MAX_SPEED = 2;
+    public static final double SPRITE_MAX_FORCE = 0.1;
 
     // distance at which the sprite moves slower towards the target 
-    public static double SPRITE_SLOW_DOWN_DISTANCE = 100; 
+    public static final double SPRITE_SLOW_DOWN_DISTANCE = 100; 
 
-    public static double GRAVITY = 0.2;
-    public static double JUMP_VELOCITY = -8;
+    public static final double GRAVITY = 0.2;
+    public static final double JUMP_VELOCITY = -8;
+    public static final double SPRING_JUMP_VELOCITY = -16;
 
-    public static double PLATFORM_WIDTH = 70;
-    public static double PLATFORM_HIGHT = 10;
 
-    public static double SHIFT_LINE = 200;
-    public static double BASE_LINE = 500;
+    public static final double PLATFORM_WIDTH = 70;
+    public static final double PLATFORM_HIGHT = 10;
 
-    public static double RIGHT_ARROW_SPEED = 7;
-    public static double LEFT_ARROW_SPEED = -RIGHT_ARROW_SPEED;
+    public static final double SHIFT_LINE = 200;
+    public static final double BASE_LINE = 500;
+
+    public static final double RIGHT_ARROW_SPEED = 7;
+    public static final double LEFT_ARROW_SPEED = -RIGHT_ARROW_SPEED;
 
     public static final double DIFFICULTY_INCREASE = 1200;
     public static final double BUFFER = 10;
     public static final double MAX_SPAWNDISTANCE = 110;
+    public static final double SPRINGS_WIDTH = 20;
+    public static final double SPRING_HEIGHT = 10;
 
-    public static String HIGHSCORE_FILENAME = "v3\\src\\main\\java\\doodlejump\\highscore.txt";
+    public static final String HIGHSCORE_FILENAME = "v3\\src\\main\\java\\doodlejump\\highscore.txt";
 
 
 }
